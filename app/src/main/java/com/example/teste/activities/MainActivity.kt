38780f5f5e5c6ccc837f56894d52e5e39acbf337
Mateus Tostes Cruz <58236280+mateustostes@users.noreply.github.com>
2021-01-07 18:34:22 -0300

    arredondando bordas

--- app/src/main/java/com/example/teste/activities/MainActivity.kt
+++ app/src/main/java/com/example/teste/activities/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.teste.activities
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -10,6 +11,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.teste.R
+
 
 class MainActivity : AppCompatActivity() {
 
